f43cdb6e2b0ffd0c722ae378b7e0fb3bc84f2a8c
ljeon0002 <lukehjeon@gmail.com>
2025-06-03 12:54:36 -0700

    added code to play music

--- src/main/java/com/finalproject/CsaProject.java
+++ src/main/java/com/finalproject/CsaProject.java
@@ -5,9 +5,20 @@ public class CsaProject {
     public static void main(String[] args) {
         Finch myFinch = new Finch();
 
-        myFinch.setMove("F", 200, 100);
+        myFinch.playNote(60, 16);
+        myFinch.pause(0.6);
+
+        // myFinch.playNote(58, 16);
+        // myFinch.pause(0.3);
+
+        myFinch.playNote(60, 16);
+        myFinch.pause(0.6);
+
+        myFinch.playNote(55, 16);
+        myFinch.pause(0.9);
 
         myFinch.stopAll();
         myFinch.disconnect();
     }
 }
+ 
